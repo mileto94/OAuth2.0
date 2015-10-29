38054a7fdac4888abddead109f6dca11d7a96852
milka <mileto94@abv.bg>
2015-10-29 10:40:45 +0200

    Refactor html files. Add GitHub login(has bugs)

--- project.py
+++ project.py
@@ -56,8 +56,10 @@ def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
-    # return "The current session state is %s" % login_session['state']
-    return render_template('login.html', STATE=state)
+    github = json.loads(open(
+        'github_client_secrets.json', 'r').read()
+    )['web']['app_id']
+    return render_template('login.html', STATE=state, github=github)
 
 
 @app.route('/gconnect', methods=['POST'])
@@ -167,8 +169,6 @@ def fbconnect():
     app_secret = json.loads(open(
         'fb_client_secrets.json', 'r').read()
     )['web']['app_secret']
-    # url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange\
-    # _token&client_id={}&client_secret={}&fb_exchange_token={}'.format(
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
         app_id, app_secret, access_token)
     h = httplib2.Http()
@@ -215,68 +215,58 @@ def fbconnect():
     output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
     flash("you are now logged in as %s" % login_session['username'])
     return output
-# def fbconnect():
-#     if request.args.get('state') != login_session['state']:
-#         response = make_response(json.dumps('Invalid state parameter.'), 401)
-#         response.headers['Content-Type'] = 'application/json'
-#         return response
-#     access_token = request.data
-#     print "access token received %s " % access_token
 
-#     app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
-#         'web']['app_id']
-#     app_secret = json.loads(
-#         open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-#     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
-#         app_id, app_secret, access_token)
-#     h = httplib2.Http()
-#     result = h.request(url, 'GET')[1]
 
-#     # Use token to get user info from API
-#     userinfo_url = "https://graph.facebook.com/v2.4/me"
-#     # strip expire tag from access token
-#     token = result.split("&")[0]
+@app.route('/ghconnect', methods=['POST'])
+def ghconnect():
+    # # Validate state token
+    # if request.args.get('state') != login_session['state']:
+    #     response = make_response(json.dumps('Invalid state parameter.'), 401)
+    #     response.headers['Content-Type'] = 'application/json'
+    #     return response
+    # Obtain authorization code
+    # access_token = request.data
 
-#     url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
-#     h = httplib2.Http()
-#     result = h.request(url, 'GET')[1]
-#     # print "url sent for API access:%s"% url
-#     # print "API JSON result: %s" % result
-#     data = json.loads(result)
-#     login_session['provider'] = 'facebook'
-#     login_session['username'] = data["name"]
-#     login_session['email'] = data["email"]
-#     login_session['facebook_id'] = data["id"]
+    app_id = json.loads(open(
+        'github_client_secrets.json', 'r').read()
+    )['web']['app_id']
+    app_secret = json.loads(open(
+        'github_client_secrets.json', 'r').read()
+    )['web']['app_secret']
+    url = 'https://github.com/login/oauth/authorize?scope=user:email&client_id=%s' % (
+        app_id)
 
-#     # The token must be stored in the login_session in order to properly logout, let's strip out the information before the equals sign in our token
-#     stored_token = token.split("=")[1]
-#     login_session['access_token'] = stored_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    # data = json.loads(result)
+    # print(result)
+    # login_session['provider'] = 'github'
+    # login_session['username'] = data['user:name']
+    # login_session['email'] = data['user:email']
+    # login_session['facebook_id'] = data['user:id']
+    # # Get user picture
+    # login_session['picture'] = data['user:picture']
 
-#     # Get user picture
-#     url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=200&width=200' % token
-#     h = httplib2.Http()
-#     result = h.request(url, 'GET')[1]
-#     data = json.loads(result)
+    # # The token must be stored in the login_session in order to properly logout, let's strip out the information before the equals sign in our token
+    # login_session['access_token'] = access_token
 
-#     login_session['picture'] = data["data"]["url"]
+    # # see if user exists
+    # user_id = getUserID(login_session['email'])
+    # if not user_id:
+    #     user_id = createUser(login_session)
+    # login_session['user_id'] = user_id
 
-#     # see if user exists
-#     user_id = getUserID(login_session['email'])
-#     if not user_id:
-#         user_id = createUser(login_session)
-#     login_session['user_id'] = user_id
-
-#     output = ''
-#     output += '<h1>Welcome, '
-#     output += login_session['username']
-
-#     output += '!</h1>'
-#     output += '<img src="'
-#     output += login_session['picture']
-#     output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
-
-#     flash("Now logged in as %s" % login_session['username'])
-#     return output
+    # output = ''
+    # output += '<h1>Welcome, '
+    # output += login_session['username']
+    # output += '!</h1>'
+    # output += '<img src="'
+    # output += login_session['picture']
+    # output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    # flash("you are now logged in as %s" % login_session['username'])
+    # return output
+    # return redirect(url_for('showRestaurants'))
+    return result
 
 
 # DISCONNECT - Revoke a current user's token and reset their login_session
